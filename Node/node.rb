a1712fdc6020cf322c46f6fe3074a9911ef3f10b
Shaunty Rashidi <shauntyrashidi@Shaunty-Rashidis-Computer.local>
2015-11-13 16:17:23 -0800

    breadth_first_search method up and running

--- Node/node.rb
+++ Node/node.rb
@@ -11,14 +11,14 @@ class Node
     if child.value < @value
       if @left_child.nil?
         @left_child = child
-        @left_child.parent = self   #questionable
+        @left_child.parent = self
       else
         @left_child.child_assign(child)
       end
     else
       if @right_child.nil?
         @right_child = child
-        @right_child.parent = self   #questionable
+        @right_child.parent = self
       else
         @right_child.child_assign(child)
       end
@@ -40,12 +40,14 @@ class Node
         @left_child
       else
         puts "Error: No left child for this node!"
+        nil
       end
     elsif direction == "right"
       unless @right_child.nil?
         @right_child
       else
         puts "Error: No right child for this node!"
+        nil
       end
     else
       puts "Error: Invalid direction input!"
@@ -67,4 +69,51 @@ def build_tree(array)
   parent
 end
 
-puts build_tree([1,4,6,7,2,4,9,11]).inspect
+def display_tree(tree_parent) # Need to work on this
+end
+
+def breadth_first_search(parent_node,target_value)
+  search_array = []
+  parent = parent_node
+  return parent if parent.value == target_value
+  if parent.left_child
+    if parent.left_child == target_value
+      return parent.left_child
+    else
+      search_array << parent.left_child
+    end
+  end
+  if parent.right_child
+    if parent.right_child == target_value
+      return parent.right_child
+    else
+      search_array << parent.right_child
+    end
+  end
+  until search_array.size == 0
+    parent = search_array.shift
+    return parent if parent.value == target_value
+    if parent.left_child
+      if parent.left_child == target_value
+        return parent.left_child
+      else
+        search_array << parent.left_child
+      end
+    end
+    if parent.right_child
+      if parent.right_child == target_value
+        return parent.right_child
+      else
+        search_array << parent.right_child
+      end
+    end
+  end
+  nil
+end
+
+tree = build_tree([1,4,6,7,2,4,9,11])
+puts tree.inspect
+puts "\n"
+puts breadth_first_search(tree,11)
+puts "\n"
+puts breadth_first_search(tree,12).inspect
